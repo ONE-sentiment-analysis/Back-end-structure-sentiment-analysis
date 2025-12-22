25806a0e0056456d914d2d42fc35fbddcb06c32f
Cauan Henrique <cauanhenrique56@gmail.com>
2025-12-22 04:24:03 -0300

    Feature da classe SentimentController e AvaliacaoRepository e Updates nas classes IdReferencia, AnaliseSentimento e SentimentResponse- Adição do "@Get" e da query "buscarPorIdProduto".

--- src/main/java/br/com/one/sentiment_analysis/controller/SentimentController.java
+++ src/main/java/br/com/one/sentiment_analysis/controller/SentimentController.java
@@ -1,20 +1,30 @@
 package br.com.one.sentiment_analysis.controller;
 
 import br.com.one.sentiment_analysis.dto.request.SentimentAnalysisRequest;
+import br.com.one.sentiment_analysis.dto.response.SentimentItemResponse;
 import br.com.one.sentiment_analysis.dto.response.SentimentResponse;
+import br.com.one.sentiment_analysis.model.AnaliseSentimento;
+import br.com.one.sentiment_analysis.model.AvaliacaoRepository;
 import br.com.one.sentiment_analysis.service.ExternalApiService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/sentiment")
 public class SentimentController {
 
+    @Autowired
+    private AvaliacaoRepository repository;
+
     private final ExternalApiService sentimentService;
+    private final Integer tamanhoPaginacao = 10;
 
     public SentimentController(ExternalApiService sentimentService) {
         this.sentimentService = sentimentService;
@@ -25,5 +35,30 @@ public class SentimentController {
     public SentimentResponse analisarComentario (@RequestBody SentimentAnalysisRequest texto)
             throws IOException, InterruptedException {
         return sentimentService.analisar(texto);
+    }
+
+    @GetMapping
+    public ResponseEntity<SentimentResponse> procurarAvaliacoes(
+            @RequestParam Long idProduto,
+            @PageableDefault(size = tamanhoPaginacao) Pageable pageable) {
+
+        Page<AnaliseSentimento> page = repository.buscarPorIdProduto(idProduto, pageable);
+
+        List<SentimentItemResponse> itens = page.getContent().stream()
+                .map(analise -> new SentimentItemResponse(
+                        analise.getIdReferencia().getValor(),
+                        analise.getTexto().getValor(),
+                        analise.getPrevisao().name(),
+                        analise.getProbabilidade().getValor()
+                ))
+                .toList();
+
+        SentimentResponse response = new SentimentResponse(
+                "SUCESSO",
+                (int) page.getTotalElements(),
+                itens
+        );
+
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/br/com/one/sentiment_analysis/model/AvaliacaoRepository.java
+++ src/main/java/br/com/one/sentiment_analysis/model/AvaliacaoRepository.java
@@ -1,5 +1,14 @@
 package br.com.one.sentiment_analysis.model;
 
-public interface AvaliacaoRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface AvaliacaoRepository extends JpaRepository<AnaliseSentimento, Long> {
+    @Query("SELECT a FROM AnaliseSentimento a WHERE a.idReferencia.valor LIKE CONCAT('prod_', :idProduto, '_review_%')")
+    Page<AnaliseSentimento> buscarPorIdProduto(@Param("idProduto") Long idProduto, Pageable pageable);
 }
