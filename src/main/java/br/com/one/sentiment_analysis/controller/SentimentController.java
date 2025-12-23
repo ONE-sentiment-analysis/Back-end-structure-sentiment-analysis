1dfca251fc3b19b5ae986521ac3948831855cb07
pablo <stevaopabllo@gmail.com>
2025-12-23 16:41:41 -0300

    merge swagger

--- src/main/java/br/com/one/sentiment_analysis/controller/SentimentController.java
+++ src/main/java/br/com/one/sentiment_analysis/controller/SentimentController.java
@@ -6,6 +6,11 @@ import br.com.one.sentiment_analysis.dto.response.SentimentResponse;
 import br.com.one.sentiment_analysis.model.AnaliseSentimento;
 import br.com.one.sentiment_analysis.model.AvaliacaoRepository;
 import br.com.one.sentiment_analysis.service.ExternalApiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,7 +22,8 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/sentiment")
+@RequestMapping("/api/v1/sentiment")
+@Tag(name = "Endpoint para realizar análise de sentimentos", description = "Retorna probabilidade e acurácia do comentário")
 public class SentimentController {
 
     @Autowired
@@ -30,14 +36,35 @@ public class SentimentController {
         this.sentimentService = sentimentService;
     }
 
-
     @PostMapping
-    public SentimentResponse analisarComentario (@RequestBody SentimentAnalysisRequest texto)
+    @Operation(summary = "Analisar comentário", description = "Recebe um texto e retorna análise de sentimento")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Comentário analisado com sucesso",
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                            value = "{ \"previsao\": \"positivo\", \"probabilidade\": 0.94 }"
+                    )
+            )
+    )
+    public SentimentResponse analisarComentario(@RequestBody SentimentAnalysisRequest texto)
             throws IOException, InterruptedException {
         return sentimentService.analisar(texto);
     }
 
     @GetMapping
+    @Operation(summary = "Procurar avaliações", description = "Busca avaliações de um produto por ID com paginação")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Lista de avaliações retornada com sucesso",
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                            value = "{ \"status\": \"SUCESSO\", \"total\": 2, \"itens\": [ { \"id\": 1, \"texto\": \"Ótimo produto\", \"previsao\": \"positivo\", \"probabilidade\": 0.95, \"dataProcessamento\": \"2025-12-23\" } ] }"
+                    )
+            )
+    )
     public ResponseEntity<SentimentResponse> procurarAvaliacoes(
             @RequestParam Long idProduto,
             @PageableDefault(size = TAMANHO_PAGINACAO) Pageable pageable) {
